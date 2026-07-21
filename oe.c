e883a267c1e376a2d8f8b625aa83dd7503692bd8
Okan Akyüz <okan.akyuz@example.com>
2026-07-21 13:01:02 +0300

    Enable Raw Mode

--- oe.c
+++ oe.c
@@ -1,8 +1,28 @@
 #include <stdlib.h>
-#include <stdio.h>
+#include <termios.h>
+#include <unistd.h>
+
+struct termios orig_termios;
+
+void 
+disableRawMode(void) {
+	tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
+
+void
+enableRawMode(void) {
+	tcgetattr(STDIN_FILENO, &orig_termios);
+	atexit(disableRawMode);
+	struct termios raw = orig_termios;
+	raw.c_lflag &= ~(ECHO | ICANON);
+	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
 
 int 
 main (int argc, char **argv) {
-	printf("Welcome to OE editor\n");
+	enableRawMode();
+	char c;
+	while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+	}
 	return EXIT_SUCCESS;
 }
