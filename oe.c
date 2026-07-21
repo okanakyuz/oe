53e206f91ce080f44487a0caf664116bd6cda78d
Okan Akyüz <okan.akyuz@example.com>
2026-07-21 13:03:24 +0300

    Rewrite prinfing

--- oe.c
+++ oe.c
@@ -7,44 +7,45 @@
 struct termios orig_termios;
 
 void 
-disableRawMode(void) {
+disableRawMode(void) 
+{
 	tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
 }
 
 void
-enableRawMode(void) {
+enableRawMode(void) 
+{
 	tcgetattr(STDIN_FILENO, &orig_termios);
 	atexit(disableRawMode);
 	struct termios raw = orig_termios;
 	raw.c_iflag &= ~(BRKINT| ICRNL | INPCK | ISTRIP | IXON);
 	raw.c_oflag &= ~(OPOST);
-	raw.c_cflag |= ~(CS8);
+	raw.c_cflag |= (CS8);
 	raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
 	raw.c_cc[VMIN] = 0;
 	raw.c_cc[VTIME] = 1;
 	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
+void 
+editorRefreshScreen(void)
+{
+	if(write(STDOUT_FILENO, "\x1b[2J", 4) == -1) exit(1);
+	if(write(STDOUT_FILENO, "\x1b[H", 3) == -1) exit(1);
+}
+
 int 
-main (int argc, char **argv) {
+main (int argc, char **argv) 
+{
 	enableRawMode();
 	while(1)
 	{
+		editorRefreshScreen();
 		char c= '\0';
-		int nread = read(STDIN_FILENO, &c, 1);
-		if (nread == 1)
-		{
-			if(iscntrl(c))
-			{
-				printf("%d\r\n", c);
-			}
-			else
-			{
-				printf("%d ('%c')\r\n", c, c);
-			}
-		}
+		if(read(STDIN_FILENO, &c, 1) == -1) exit(1);
 		if (c == 'q')
 		{
+			editorRefreshScreen();
 			break;
 		}
 	}
